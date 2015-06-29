f303c5f431a54127916eef672c1c16951a05b249
Liam Hackett <liamjhackett@gmail.com>
2015-06-30 00:31:26 +0100

    Preparing OpenGL class
    Preparing to use OpenGL's SurfaceView class within my app.

---
+++ app/src/main/java/liamhackett/com/opengltest/MyActivity.java
@@ -0,0 +1,19 @@
+package liamhackett.com.opengltest;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+/**
+ * Created by Liam on 30/06/2015.
+ */
+public class MyActivity extends Activity {
+
+    private GLSurfaceView mGLSurfaceView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/liamhackett/com/opengltest/OpenGLSurface.java
@@ -0,0 +1,12 @@
+package liamhackett.com.opengltest;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+
+/**
+ * Created by Liam on 30/06/2015.
+ */
+public class OpenGLSurface {
+
+
+}
