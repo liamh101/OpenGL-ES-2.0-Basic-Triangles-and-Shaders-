735b932866c0cbfd3ed3f3ee11b18114af55e280
Liam Hackett <liamjhackett@gmail.com>
2015-06-30 03:15:07 +0100

    Added GLSurface
    Added code to initiate OpenGL ES 2.0 renderer.

--- app/src/main/java/liamhackett/com/opengltest/MyActivity.java
+++ app/src/main/java/liamhackett/com/opengltest/MyActivity.java
@@ -1,6 +1,9 @@
 package liamhackett.com.opengltest;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
@@ -15,5 +18,33 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState){
 
         super.onCreate(savedInstanceState);
+
+        mGLSurfaceView = new GLSurfaceView(this);
+
+        //Check to see if device supports OpenGL ES 2.0
+
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final Boolean supportsES2 = configurationInfo.reqGlEsVersion >= 0x20000;
+
+        if(supportsES2){
+
+            //Request GL compatible context
+            mGLSurfaceView.setEGLContextClientVersion(2);
+
+            //Set Renderer for App
+            //mGLSurfaceView.setRenderer();
+
+
+        }
+        else{
+            // Insert custom renderer here.
+            return;
+        }
+
+        setContentView(mGLSurfaceView);
+
+
+
     }
 }
